2b3f112ceb3ec2da8e8720fc50b1b36d907a4e1a
ChrisOtt0 <sophusott@gmail.com>
2022-11-30 23:43:44 +0100

    Removed ViewModels as they overcomplicated the work for what they were worth.

---
+++ Client/Pages/DeleteAccountPage.razor.cs
@@ -0,0 +1,57 @@
+﻿using blazorSBIFS.Shared.DataTransferObjects;
+using Microsoft.AspNetCore.Components;
+
+namespace blazorSBIFS.Client.Pages
+{
+	public partial class DeleteAccountPage
+	{
+        string baseUrl = "User/";
+
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+        protected override async void OnInitialized()
+        {
+            if (_token.Jwt == string.Empty) _nav.NavigateTo("login");
+        }
+
+        public async void Submit()
+        {
+            if (Email == string.Empty || Email == "")
+            {
+                Message = "If you want to delete your account, please enter your email.";
+                StateHasChanged();
+                return;
+            }
+
+            if (Password == String.Empty || Password == "")
+            {
+                Message = "If you want to delete your account, please enter your password.";
+                StateHasChanged();
+                return;
+            }
+
+            Message = "Deleting user..";
+            StateHasChanged();
+
+            string url = "Delete";
+            IJson data = new UserLoginDto
+            {
+                Email = this.Email,
+                Password = this.Password
+            };
+            HttpResponseMessage response = await _http.Delete(baseUrl + url, data);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Message = await response.Content.ReadAsStringAsync();
+                StateHasChanged();
+                return;
+            }
+
+            _token.Jwt = string.Empty;
+            _nav.NavigateTo("/");
+        }
+    }
+}
---
+++ Client/Pages/GroupPage.razor.cs
@@ -0,0 +1,59 @@
+﻿using blazorSBIFS.Shared.DataTransferObjects;
+using blazorSBIFS.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace blazorSBIFS.Client.Pages
+{
+	public partial class GroupPage
+	{
+        string baseUrl = "Group/";
+
+        [Parameter]
+        public int GroupID { get; set; }
+        public string GroupName { get; set; } = "not found.";
+        public Group? Group { get; set; }
+
+        protected override async void OnInitialized()
+        {
+            if (_token.Jwt == string.Empty) _nav.NavigateTo("login");
+
+            string url = "ReadOne";
+            IJson data = new GroupDto
+            {
+                GroupID = this.GroupID
+            };
+            HttpResponseMessage response = await _http.Post(baseUrl + url, data);
+
+            // catching errors
+            if (!response.IsSuccessStatusCode)
+            {
+                GroupID = 0;
+                return;
+            }
+
+            Group? g = await response.Content.ReadFromJsonAsync<Group>();
+            if (g == null)
+            {
+                GroupID = 0;
+                return;
+            }
+
+            Group = g;
+            GroupName = g.Name;
+            StateHasChanged();
+        }
+
+        public void UpdateName()
+        {
+            GroupName = Group.Name;
+            StateHasChanged();
+        }
+
+        public void RemoveParticipant(User user)
+        {
+            Group.Participants.Remove(user);
+            StateHasChanged();
+        }
+    }
+}
