9fcb125615006075ed5c7267f0edd03143bae4f4
ChrisOtt0 <sophusott@gmail.com>
2022-11-30 11:17:49 +0100

    Added remove Participant functionality and Read Participants functionality to GroupController.

--- Server/Controllers/GroupController.cs
+++ Server/Controllers/GroupController.cs
@@ -47,6 +47,17 @@ namespace blazorSBIFS.Server.Controllers
             return Ok(groups);
         }
 
+        [HttpPost("ReadParticipants"), Authorize(Roles = "user")]
+        public async Task<ActionResult<List<User>>> ReadParticipants(GroupDto request)
+        {
+            var participants = await _context.Groups
+                .Where(g => g.GroupID == request.GroupID)
+                .Select(g => g.Participants)
+                .ToListAsync();
+
+            return Ok(participants);
+        }
+
         [HttpPost("Create"), Authorize(Roles = "user")]
         public async Task<ActionResult<List<Group>>> Create()
         {
@@ -96,6 +107,36 @@ namespace blazorSBIFS.Server.Controllers
                 return BadRequest("User is already a participant.");
 
             group.Participants.Add(participant.User);
+            await _context.SaveChangesAsync();
+
+            return Ok(group);
+        }
+
+        [HttpPut("RemoveParticipant"), Authorize(Roles = "user")]
+        public async Task<ActionResult<Group>> RemoveParticipant(GroupDto groupRequest, EmailDto participantRequest)
+        {
+            int userID = _userService.GetUserID();
+            var group = await _context.Groups
+                .Where(g => g.GroupID == groupRequest.GroupID)
+                .Include(g => g.Participants)
+                .FirstOrDefaultAsync();
+            if (group == null)
+                return BadRequest("No such group.");
+
+            if (group.OwnerID != userID)
+                return Unauthorized("Only the group owner can remove participants.");
+
+            var participant = await _context.UserLogins
+                .Where(u => u.Email == participantRequest.Email)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync();
+            if (participant == null)
+                return BadRequest("No such user");
+
+            if (!group.Participants.Contains(participant.User))
+                return BadRequest("User is not a participant in the selected group.");
+
+            group.Participants.Remove(participant.User);
             await _context.SaveChangesAsync();
 
             return Ok(group);
