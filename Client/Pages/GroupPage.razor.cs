f28674395194f67795ba23f0055ac1a5e15afc83
riccardigithub <riccardiroca@gmail.com>
2022-12-05 07:30:28 +0100

    ReadGroupData() and DeleteGroup() in GroupPage.razor.cs

--- Client/Pages/GroupPage.razor.cs
+++ Client/Pages/GroupPage.razor.cs
@@ -14,6 +14,12 @@ namespace blazorSBIFS.Client.Pages
         public string GroupName { get; set; } = "not found.";
         public Group? Group { get; set; }
 
+        /// <summary>
+        /// It gets the group from the database and sets the GroupName to the group's name
+        /// </summary>
+        /// <returns>
+        /// A Group object.
+        /// </returns>
         protected override async void OnInitialized()
         {
             if (_token.Jwt == string.Empty) _nav.NavigateTo("login");
@@ -60,12 +66,19 @@ namespace blazorSBIFS.Client.Pages
             Group.Participants.Add(user);
             StateHasChanged();
         }
+        /// <summary>
+        /// The function is called when the user clicks the save button. It sends a post request to the server with the
+        /// group's name and participants. If the request is successful, the group's name is updated
+        /// </summary>
+        /// <returns>
+        /// The method returns a list of all the groups that the user is a part of.
+        /// </returns>
         public void SaveChanges() //Add participants method is not implemented yet into the .razor file
         {
             string url = "Update";
             IJson data = new GroupDto
             {
-                GroupID = this.GroupID,
+                GroupID = GroupID,
                 Name = Group.Name,
                 Participants = Group.Participants
             };
@@ -77,5 +90,56 @@ namespace blazorSBIFS.Client.Pages
             }
             UpdateName();
     }
-}
+        /// <summary>
+        /// It reads the group data from the server and updates the name of the group
+        /// </summary>
+        /// <returns>
+        /// A Group object.
+        /// </returns>
+        public void ReadGroupData()
+        {
+            string url = "ReadOne";
+            IJson data = new GroupDto
+            {
+                GroupID = GroupID
+            };
+            HttpResponseMessage response = _http.Post(baseUrl + url, data).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                GroupID = 0;
+                return;
+            }
+            Group? g = response.Content.ReadFromJsonAsync<Group>().Result;
+            if (g == null)
+            {
+                GroupID = 0;
+                return;
+            }
+            Group = g;
+            UpdateName();
+        }
+        /// <summary>
+        /// The function takes the GroupID from the view model and sends it to the server. If the server returns a success
+        /// code, the user is navigated to the groups page
+        /// </summary>
+        /// <returns>
+        /// A HttpResponseMessage
+        /// </returns>
+        public void DeleteGroup()
+        {
+            string url = "Delete";
+            IJson data = new GroupDto
+            {
+                GroupID = GroupID
+            };
+            HttpResponseMessage response = _http.Post(baseUrl + url, data).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                GroupID = 0;
+                return;
+            }
+            _nav.NavigateTo("groups");
+        }
+        
+    }
 }
